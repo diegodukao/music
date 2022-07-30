d75fac5977a348141948e13c507f1f4fb14b1adb
Diego Moreira Guimarães <diegodukao@gmail.com>
2022-07-30 17:29:09 -0400

    funk microtonal updated

--- 30dias30beats/07-funk-microtonal.py
+++ 30dias30beats/07-funk-microtonal.py
@@ -1,16 +1,20 @@
+Root.default = "c"
+Clock.set_time(-1)
+Scale.default = "major"
+var.r= var(P[0,0.5,1,0.8],dur=[1.5,1,0.5,1])
+p1 >> pluck(var.r +P[0]-7, dur=[1/2,1/2, rest(1/2),1/2, 1/2,1/4,1/4, 1/2,1/2,
+                      rest(1/2),1/2, 1/2,1/2, 1/2,1/4,1/4, 1/2,1/2,], pan=0, amp=0.7)
+b1 >> bass(var.r + P[0], dur=[3/4,rest(1/4), rest(1),1/2,1/2, rest(3/4),rest(1/4),    3/4,rest(1/4), rest(1),1/2,1/2, 1/2,rest(1/2),], sus=[3/4,rest(1/4), rest(1),1/2,1/2, rest(3/4),rest(1/4),    3/4,rest(1/4), rest(1),1/2,1/2, 1,rest(1/2),], amp=0.5, lpf=0)
+
 d1 >> play("o", sample=0, dur=[rest(3/4),1/4, rest(1/2),1/2, rest(1),1], drive=0.05, rate=1)
 d2 >> play("X", sample=1,dur=[3/4,rest(1/4), rest(1),1/2,1/2, rest(3/4),1/4,    3/4,rest(1/4), rest(1),1/2,1/2, rest(3/4),rest(1/4),], amp=0.8)
 f3 >> play("V", sample=0,dur=8, room=1, mix=0.5)
 f4 >> play("x", sample=0,dur=8, room=1, mix=0.5)
 f5 >> play("X", sample=1,dur=8, room=1, mix=0.5)
 
-Clock.set_time(-1)
-
-var.r= var(P[0,0.5,1,0.8],dur=[1.5,1,0.5,1])
-p1 >> pluck(var.r +P[0]-7, dur=[1/2,1/2, rest(1/2),1/2, 1/2,1/4,1/4, 1/2,1/2,
-                      rest(1/2),1/2, 1/2,1/2, 1/2,1/4,1/4, 1/2,1/2,], pan=0, amp=0.7)
-b1 >> bass(var.r + P[0], dur=[3/4,rest(1/4), rest(1),1/2,1/2, rest(3/4),rest(1/4),    3/4,rest(1/4), rest(1),1/2,1/2, 1/2,rest(1/2),], sus=[3/4,rest(1/4), rest(1),1/2,1/2, rest(3/4),rest(1/4),    3/4,rest(1/4), rest(1),1/2,1/2, 1,rest(1/2),], amp=0.5, lpf=0)
 p2 >> play("[ss]", amplify=var([1,0], dur=[4,2,1,1]), pan=0.4, amp=0.5)
+
+
 
 p2.stop()
 
